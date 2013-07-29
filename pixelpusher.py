72e59b6385e531b027a7b7868b34b5a2c49aea63
graham <graham@todd.local>
2013-07-28 19:37:25 -0700

    first code, still need to add a readme.

---
+++ pixelpusher.py
@@ -0,0 +1,53 @@
+import struct
+import socket
+
+def bound(value, bottom, top):
+    if value < bottom:
+        return bottom
+    if value > top:
+        return top
+    return value
+
+## Doing things the functional way.
+
+def pixel(red, green, blue):
+    red = bound(red, 0, 255)
+    green = bound(green, 0, 255)
+    blue = bound(blue, 0, 255)
+
+    return struct.pack('!BBB', red, green, blue)
+
+def build_strip(id=0):
+    return [struct.pack('!xxxxB', id)]
+
+def send_strip(ip, strip):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(ip, ''.join(strip))
+
+## End functional stuff.
+
+
+## Do things the Object way.
+## This is a little less efficient if you want to update multiple strips
+## in a single packet, but it also means you can think of strips as separate
+## from their controllers.
+
+class Strip(object):
+    def __init__(self, ip, sid):
+        self.ip = ip
+        self.sid = sid
+        self.pixel_map = [pixel(0, 0, 0) for i in range(0, 240)]
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.dirty = False
+    
+    def update(self):
+        strip = build_strip(self.sid)
+        self.sock.sendto(''.join(strip + self.pixel_map), self.ip)
+        self.dirty = False
+
+    def set_pixel(self, index, red, green, blue):
+        self.pixel_map[index] = pixel(red, green, blue)
+        self.dirty = True
+
+## That's really the end of the OO stuff.
+
---
+++ service.py
@@ -0,0 +1,81 @@
+from pixelpusher import Strip, bound
+import random
+import time
+
+class Animation(object):
+    def __init__(self, service):
+        self.strip, self.dot = service.get_open_dot()
+        self.init()
+    def __str__(self):
+        return "<Animation: %s strip:%i | %i>" % (
+            self.__class__.__name__, self.strip.sid, self.dot)
+    def init(self):
+        pass
+    def step(self):
+        pass
+    def finish(self, service):
+        service.release_dot(self.strip, self.dot)
+
+class Ping(Animation):
+    def init(self):
+        self.level = 1.0
+        self.red = bound(random.randint(-64, 255), 0, 255)
+        self.green = bound(random.randint(-64, 255), 0, 255)
+        self.blue = bound(random.randint(-64, 255), 0, 255)        
+
+    def step(self):
+        self.strip.set_pixel(self.dot, int(self.red * self.level), int(self.green * self.level), int(self.blue * self.level))
+
+        if self.level > 0.25:
+            self.level -= 0.005
+        elif self.level <= 0.25:
+            self.level -= 0.002
+
+        if self.level > 0.0:
+            return True
+        else:
+            self.strip.set_pixel(self.dot,0,0,0)
+            return False
+
+class Service(object):
+    def __init__(self, strips):
+        self.strips = strips
+        self.animations = []
+        self.used_pairs = [[] for i in strips]
+
+    def get_open_dot(self):
+        small_index = 0
+        for index, i in enumerate(self.used_pairs):
+            if len(i) < len(self.used_pairs[small_index]):
+                small_index = index
+
+        dot = random.randint(0, 240)
+        dot = bound(dot, 0, 239)
+        used = self.used_pairs[small_index]
+
+        self.used_pairs[small_index].append(dot)
+        return self.strips[small_index], dot
+
+    def release_dot(self, strip, dot):
+        self.used_pairs[strip.sid].remove(dot)
+
+    def step(self):
+        self.animations = filter(lambda x: x.step(), self.animations)
+
+if __name__ == '__main__':
+    ip = ('192.168.0.99', 9897)
+    x, y = Strip(ip, 0), Strip(ip, 1)
+    s = Service([x, y])
+
+    index = 0
+    while True:
+        index += 1
+        s.step()
+        time.sleep(0.005)
+
+        if index % 16 == 0:
+            s.animations.append( Ping(s) )
+
+        for strip in s.strips:
+            strip.update()
+
