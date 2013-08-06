0d33feb2b1714df45e038282b512bedeab62da44
Graham Abbott <graham.abbott@gmail.com>
2013-08-05 21:00:01 -0700

    fixed a direction error and moved service2 to service.

--- blitter.py
+++ blitter.py
@@ -6,23 +6,20 @@ import cPickle
 
 from pixelpusher import pixel, build_strip, send_strip
 
-IP = '192.168.0.99'
-PORT = 9897
+import sys
+
+if sys.argv > 1:
+    IP = sys.argv[1]
+else:
+    IP = '192.168.0.99'
+    PORT = 9897
     
 MAX = 255
 MID = 128
 OFF = 0
 
+FRAME_MASTER = 'master'
 FRAME_KEY = 'frame'
-
-def rand_pixel():
-    return pixel(
-        random.randint(OFF, MAX),
-        random.randint(OFF, MAX),
-        random.randint(OFF, MAX))
-
-def rand_frame():
-    return [rand_pixel() for i in range(120 * 8)]
 
 def prep_client(client):
     client.delete(FRAME_KEY)
@@ -32,14 +29,11 @@ def main():
     prep_client(client)
 
     pixel_width = 120
-    on = True
-    master_delay = 0.0025
     delay = 0.0025
 
     while True:
         frame_name, frame = client.blpop(FRAME_KEY)
         frame = cPickle.loads(frame)
-
         lines = []
         
         lines.append(frame[0:pixel_width])
@@ -52,11 +46,9 @@ def main():
         lines.append(frame[pixel_width*7:pixel_width*8])
 
         for index in range(0, 4):
-            s = struct.pack('!xxxxB', index) + ''.join(lines[index*2]) + ''.join(lines[(index*2)+1])
+            s = struct.pack('!xxxxB', index) + ''.join(lines[index*2]) + ''.join(reversed(lines[(index*2)+1]))
             send_strip(''.join(s), (IP, PORT))
             time.sleep(delay)
-
-        on = not on
     
 if __name__ == "__main__":
     main()
